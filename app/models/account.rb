8862152b71e3d357650c2f0d3c24cb6556be8b46
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-10-16 02:49:03 -0700

    Not like the user can set it to nil easily

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,5 @@ class Account < ApplicationRecord
   has_many :authentications
 
   validates :name, presence: true
-  validates :metadata, presence: true
   validates :authentications, presence: true
 end
