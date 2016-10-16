38dd2517a2730aa086a254cc1f54aed5a95d6216
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-10-16 02:47:54 -0700

    Gonna need to easily access the account being used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::API
+class ApplicationController < JSONAPI::ResourceController
+  protected def current_session
+    if session.has_key?(:account_id)
+      @current_session ||= Account.new(session[:account_id])
+    end
+  end
 end
