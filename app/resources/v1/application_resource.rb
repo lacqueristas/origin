858dbb8dfd104221250ca0677b78b1d5c888a7f1
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-02-21 00:25:34 -0800

    Now we can make sure all lookups are scoped by authentication

--- app/resources/v1/application_resource.rb
+++ app/resources/v1/application_resource.rb
@@ -1,5 +1,11 @@
 module V1
   class ApplicationResource < JSONAPI::Resource
+    def self.authenticated_scope(relationship)
+      define_method(:records) do |options = {}|
+        options[:context][:current_account].public_send(relationship)
+      end
+    end
+
     def meta(_)
       {
         version: "v1"
