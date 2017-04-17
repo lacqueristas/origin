4f7c4b49eb92248fddfd4cbb36837597badf7343
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-16 23:27:14 -0700

    These will be handy

--- app/resources/v1/application_resource.rb
+++ app/resources/v1/application_resource.rb
@@ -10,6 +10,24 @@ module V1
       end
     end
 
+    def self.create_with(*keys)
+      define_singleton_method(:creatable_fields) do |context|
+        super(context) + keys
+      end
+    end
+
+    def self.update_with(*keys)
+      define_singleton_method(:updatable_fields) do |context|
+        super(context) + keys
+      end
+    end
+
+    def self.show_without(*keys)
+      define_method(:fetchable_fields) do
+        super.without(keys)
+      end
+    end
+
     def meta(_)
       {
         version: "v1"
