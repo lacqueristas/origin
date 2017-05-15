402d699892a96d4e05e9dfbe26df2720c2aede5f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-05-15 01:35:24 -0700

    Forgot to properly denote this as an abstract

--- app/resources/v1/application_resource.rb
+++ app/resources/v1/application_resource.rb
@@ -1,5 +1,7 @@
 module V1
   class ApplicationResource < JSONAPI::Resource
+    abstract
+
     def self.limited_to(key, relationship)
       define_singleton_method(:create) do |context|
         new(context[key].public_send(relationship).new, context)
